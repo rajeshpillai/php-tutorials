b4b2c87fe40d416fee3e434c9bcd95d40e6946b9
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-13 20:26:24 +0530

    Data type updates

--- tutorials/2-datatypes.php
+++ tutorials/2-datatypes.php
@@ -1,6 +1,26 @@
-<h1>Data Types </h1>
+<h1>PHP Data Types </h1>
 
-<ul>
+<div>
+  <h1>Variables</h1>
+  <div>
+  Variables #
+  A variable in any programming language is a named piece of computer memory, 
+  containing some information inside. Variables are one of the essential parts of 
+  a computer program. You can declare a variable in PHP using a $ sign followed 
+  by its name, e.g., $myVariable
+  </div>
+</div>
+
+<div>
+  <h1>Data types</h1>
+  <div>Along with the name of variables, their sizes may vary too. In most programming 
+  languages (like C++ and Java) you can use different data types for your variables. PHP, however, does not have explicit type definitions, but the type of a variable is determined by the type of the value that it is assigned, or by the type that it is cast to.</div>
+
+  <div>There are different data types for different purposes, i.e., null, boolean, 
+  integer, float, string, object, resource, and an array. Here is a brief overview of the types. For a detailed documentation and examples, see the PHP documentation.</div>
+</div>
+
+<ol>
   <li><h2>Null</h2>
     <pre>
       $foo = null;
@@ -54,4 +74,26 @@
       $bin = 0b1010; // 0b prefix is used to specify binary - binary value = 10 decimal
       var_dump($negative, $zero, $positive, $zeroPos, $hex, $bin); ?>
   </li>
-</ul>
+  <li>
+    <h2>Array</h2>
+    <pre>
+      $foo = array(1, 2, 3); // An array of integers created using array fucntion
+      $bar = ["A", true, 123 => 5]; // Short array syntax, PHP 5.4+
+      echo $bar[0]; // Returns "A"
+      echo "\n";
+      echo $bar[1]; // Returns 1 for true
+      echo "\n";
+      echo $bar[123]; // Returns 5
+    </pre>
+  </li>
+  <li>
+    <h2>String</h2>
+    <div>A string is like an array of characters. Like an array, a string can be indexed to return its individual characters:</div>
+    <pre>
+    $foo = "I am a string";
+    echo $foo; // outputs "I am a string"
+    echo "\n";
+    echo $foo[3]; // Prints 'm', the third character of the string in $foo.
+    </pre>
+  </li>
+</ol>
